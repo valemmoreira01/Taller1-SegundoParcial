fb17f154e145d22ea731bf5b5ecba9b2127f975d
valemmoreira27 <94233845+valemmoreira01@users.noreply.github.com>
2022-02-08 15:18:45 -0500

    Avance
    Vista

--- Code/proyecto/VCS/polls/urls.py
+++ Code/proyecto/VCS/polls/urls.py
@@ -1,6 +1,16 @@
+from django.http import HttpResponse
 from django.urls import path
 from .import views
 
+app_name = 'polls'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    #ex: /polls/
+    path('', views.index, name='index'),
+    #ex: /polls/1/
+    path('<int:proyecto_id>/', views.detalle, name='detalle'),
+    #ex: /polls/1/resultados/
+    path('<int:proyecto_id>/resultados/', views.resultados, name='resultados'),
+    #ex: /polls/1/calificacion/
+    path('<int:proyecto_id>/calificacion/', views.calificacion, name='calificacion'),
 ]
--- Code/proyecto/VCS/polls/views.py
+++ Code/proyecto/VCS/polls/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Proyecto
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hola mundo')
+    proyectos=Proyecto.objects.all()
+    return render(request, 'polls/index.html', {'latest_proyecto_list': proyectos})
+
+def detalle(request, proyecto_id):
+    proyecto = Proyecto.objects.get(pk=proyecto_id)
+    return render(request, 'polls/detalle.html', {'proyecto': proyecto})
+
+def resultados(request, proyecto_id):
+    return HttpResponse(f'Estas viendo la calificacion del proyecto numero: {proyecto_id}')
+
+def calificacion (request, proyecto_id):
+    return HttpResponse(f'Estas calificando el proyecto numero: {proyecto_id}')
